a8b88097ee6b0ac420b4addf1277c73a02529830
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-13 11:57:21 +0530

    start work on neighbour count funct

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,17 @@ impl Universe {
         (row * self.width + col) as usize
     }
 
-    fn live_neighbour_count() -> {
+    fn live_neighbour_count(&self, row: u32, col: u32) -> u32 {
         let mut count = 0;
-         
+        for del_row in [self.height - 1, 0, 1].iter().cloned() {
+            for del_col in [self.width - 1, 0, 1].iter().cloned() {
+                if del_row == 0 && del_col == 0 {
+                    continue;
+                }
+                let neighbour_row = ( row + del_row ) % self.height;
+                let neighbour_col = ( col + del_row ) % self.height;
+            }
+        }
     }
 
     fn tick() -> Universe { }
