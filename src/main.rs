c13f95d46707d6baafc3a5ead7d3620a3858ffc0
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-15 11:09:32 +0530

    universe can now tick

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ impl Universe {
         (row * self.width + col) as usize
     }
 
-    fn live_neighbour_count(&self, row: u32, col: u32) -> u32 {
+    fn live_neighbour_count(&self, row: u32, col: u32) -> u8 {
         let mut count = 0;
         for del_row in [self.height - 1, 0, 1].iter().cloned() {
             for del_col in [self.width - 1, 0, 1].iter().cloned() {
@@ -34,6 +34,24 @@ impl Universe {
         }
     }
 
-    fn tick() -> Universe { }
+    fn tick(&mut cells) -> Universe {
+        let mut new = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let live_neighbour_count = self.live_neighbour_count(row, col);
 
+                match (cell, live_neighbour_count) { 
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive
+                };
+
+                new[index] = cell;
+            }
+        }
+    }
+    self.cells = new;
 }
