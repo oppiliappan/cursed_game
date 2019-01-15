885b3f7ba5a36504865942efea58f050f2a2d7cb
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-15 20:28:44 +0530

    Split into modules, add basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,14 @@
+mod universe;
+
+use std::time::Duration;
+use std::thread;
+
 fn main() {
     println!("Hello, world!");
-}
-
-enum Cell {
-    Dead = 0,
-    Alive = 1
-}
-
-struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>
-}
-
-impl Universe {
-
-    fn new() -> Universe { }
-
-    fn get_index(&self, row: u32, col: u32) -> usize {
-        (row * self.width + col) as usize
+    let mut myuniverse = universe::Universe::new(10, 10);
+    for _ in 0..100 {
+        println!("{}", myuniverse);
+        thread::sleep(Duration::from_millis(1000));
+        myuniverse.tick();
     }
-
-    fn live_neighbour_count(&self, row: u32, col: u32) -> u8 {
-        let mut count = 0;
-        for del_row in [self.height - 1, 0, 1].iter().cloned() {
-            for del_col in [self.width - 1, 0, 1].iter().cloned() {
-                if del_row == 0 && del_col == 0 {
-                    continue;
-                }
-                let neighbour_row = ( row + del_row ) % self.height;
-                let neighbour_col = ( col + del_row ) % self.height;
-            }
-        }
-    }
-
-    fn tick(&mut cells) -> Universe {
-        let mut new = self.cells.clone();
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let index = self.get_index(row, col);
-                let cell = self.cells[index];
-                let live_neighbour_count = self.live_neighbour_count(row, col);
-
-                match (cell, live_neighbour_count) { 
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    (Cell::Dead, 3) => Cell::Alive
-                };
-
-                new[index] = cell;
-            }
-        }
-    }
-    self.cells = new;
 }
